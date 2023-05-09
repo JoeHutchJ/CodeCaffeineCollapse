610e37db4b259e223165a76be30d87775d8181d5
xHutchi <joehutchj@gmail.com>
2023-05-09 21:07:37 +0100

    Starting create code mechanic

---
+++ Assets/Joe Testing/QTECodeMechanic/CodeCreationController.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class CodeCreationController : MonoBehaviour
+{
+    public List<float> requests;
+
+    public int numLines = 20;
+
+    public float scrollSpeed = 0;
+
+    float QTEFrequency;
+    public GameObject CodeLinesLayout;
+
+    public RectTransform ContentBox;
+
+    public ScrollManager scrollManager;
+
+    public GameObject CodeLinePrefab;
+
+    public GameObject NoMoreCodingRequests;
+
+    public RectTransform inBoundsline;
+
+    public GameObject progressBar;
+
+    public TMP_Text RequestsCountElement;
+
+    float timeSincelastCheck = 0;
+
+    float estimatedCodeLineHeight = 3.6202f;
+
+    List<GameObject> codeLines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CodeLinesLayout = GetChildByName.Get(this.gameObject, "CodeLinesLayout");
+        ContentBox = GetChildByName.Get(this.gameObject, "Content").GetComponent<RectTransform>();
+        scrollManager = GetChildByName.Get(this.gameObject, "Scrollbar Vertical").GetComponent<ScrollManager>();
+        progressBar = GetChildByName.Get(this.gameObject, "ProgressBar");
+        RequestsCountElement = GetChildByName.Get(this.gameObject, "RequestsCount").GetComponent<TMP_Text>();
+        estimatedCodeLineHeight = CodeLinePrefab.GetComponent<RectTransform>().sizeDelta.y;
+        codeLines = new List<GameObject>();
+        generateNew();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeSincelastCheck > 0.5) {
+            timeSincelastCheck = 0;
+            checkCodeLines(codeLines);
+        }
+        timeSincelastCheck += Time.deltaTime;
+    }
+
+    public void newDifficulty(float difficulty) {
+        scrollManager.scrollSpeed = UsefulFunctions.Remap(difficulty, 0, 1, 0.8f, 1.0f);
+        numLines = (int)UsefulFunctions.Remap(difficulty, 0, 1, 10, 70);
+        QTEFrequency = UsefulFunctions.Remap(difficulty, 0, 1, 0.8f, 0.6f);
+
+    }
+
+    public void generateNew() {
+        wipeContentbox();
+        if (requests.Count >= 1) {
+            newDifficulty(requests[0]);
+        
+        scrollManager.resetScrolling();
+        string CodeString = generateGibberishCode.GenerateRandomCode(numLines);
+        string[] LineArray = CodeString.Split("\n");
+        ContentBox.sizeDelta = new Vector2(ContentBox.sizeDelta.x, LineArray.Length * estimatedCodeLineHeight + 70);
+        foreach(string Line in LineArray) {
+            addToContent(Line);
+
+        }
+        scrollManager.startScrolling();
+        } else {
+            Instantiate(NoMoreCodingRequests, CodeLinesLayout.transform);
+        }
+
+
+
+    }
+
+    public void addToContent(string Line) {
+        GameObject newCodeLine = Instantiate(CodeLinePrefab, CodeLinesLayout.transform);
+        newCodeLine.GetComponent<CreateCodeLine>().Setup(Line, QTEFrequency);
+        codeLines.Add(newCodeLine);
+
+    }
+
+    public void checkCodeLines(List<GameObject> lines) {
+        foreach (GameObject line in lines) {
+            checkCodeLine(line);
+        }
+    }
+
+    public void checkCodeLine(GameObject line) {
+        RectTransform rectTransform = line.GetComponent<RectTransform>();
+        CreateCodeLine codeLine = line.GetComponent<CreateCodeLine>();
+           if (LineIntersect(inBoundsline, rectTransform)) { 
+            codeLine.inBounds();
+        } else {
+            codeLine.outBounds();
+        }
+    }
+
+
+    public bool LineIntersect(RectTransform point, RectTransform line) {
+        Vector3[] corners = new Vector3[4];
+        line.GetWorldCorners(corners);
+        for (int i = 0; i < corners.Length; i++) {
+            corners[i] = Camera.main.WorldToScreenPoint(corners[i]);
+            if (RectTransformUtility.RectangleContainsScreenPoint(point, corners[i], Camera.main)) {
+                return true;
+            }
+        }
+        return false;
+
+
+
+
+    }
+
+
+
+    void wipeContentbox() {
+        foreach (Transform child in CodeLinesLayout.transform) {
+            Destroy(child.gameObject);
+        }
+    }
+}
---
+++ Assets/Joe Testing/QTECodeMechanic/CreateCodeLine.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class CreateCodeLine : MonoBehaviour
+{
+    public bool active;
+
+    public bool finished;
+    public TMP_Text textBox;
+
+    public Transform QTEIcon;
+
+    public TMP_Text QTEtextBox;
+
+    public string text;
+
+    public string displaytext;
+    public Boolean QTEpossible;
+
+    int currentCharindex = 0;
+
+    float timeSince = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textBox.text = displaytext;
+        if (timeSince > 0.3) {
+        addLetter();
+        timeSince = 0;
+        }
+        timeSince += Time.deltaTime;
+    }
+
+    public void Setup(string _text, float frequency) {
+        QTEIcon = transform.Find("QTE Icon");
+        textBox = transform.Find("Text").GetComponent<TMP_Text>();
+        text = _text;
+        
+        //textBox.text = text;
+
+        if (text.Contains("}")) {
+
+        } else {
+            QTEpossible = true;
+        }
+
+
+
+    }
+
+    public void addLetter() {
+        
+            if (currentCharindex >= text.Length) {
+                finished = true;
+            } else {
+            displaytext += text[currentCharindex];
+            currentCharindex++;
+            }
+        
+        
+    }
+
+    public void outBounds() {
+        active = false;
+    }
+
+    public void inBounds() {
+        active = true;
+
+        
+    }
+}
