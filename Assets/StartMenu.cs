ab96bbf6d374f9d50adfedcd8f3f7ff62a5f75bb
xHutchi <joehutchj@gmail.com>
2023-05-29 04:45:07 +0100

    hmm

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public bool enabled; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Enable() {
+        //move camera
+
+
+
+    }
+
+    public void StartGame() {
+        //move camera...
+
+        //enable UI...
+
+        //hide (hideAllchildren)
+
+        //unlock cursor
+
+    }
+}
